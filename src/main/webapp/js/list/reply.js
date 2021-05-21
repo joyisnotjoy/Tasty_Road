c208cc961f52cbc45d87fd541d86008ffacfd055
seokku <79212300+seokku@users.noreply.github.com>
2021-05-21 17:55:27 +0900

    1

--- src/main/webapp/js/list/reply.js
+++ src/main/webapp/js/list/reply.js
@@ -1,6 +1,6 @@
 
 
-
+// view.jsp 부분
 
 $(function(){
 	// 처리 후 나타나는 메시지 : 글쓰기나 글삭제 처리된 후 리스트로 돌아 오면 보여준다.
@@ -307,3 +307,160 @@ $(function(){
 	);
 	
 }); // $(function(){~}) 의 끝.
+
+// view 
+
+var str = "";
+// 	
+	str += "<div class='container'>";
+	str += "<h1>맛집 보기</h1>";
+	
+	str += "<ul class='list-group'>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>가게명</div>";
+	str += "<div class='col-md-10'>" + data.shopName + "</div>";
+	str += "</li>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>사업자번호</div>";
+	str += "<div class='col-md-10' id='viewShopNo'>" + data.shopNo + "</div>";
+	str += "</li>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>주소</div>";
+	str += "<div class='col-md-10'>";
+	str += "<pre>" + data.address + "</pre>";
+	str += "</div>";
+	str += "</li>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>소개</div>";
+	str += "<div class='col-md-10'>" + data.content + "</div>";
+	str += "</li>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>전화번호</div>";
+	str += "<div class='col-md-10'>" + data.tel + "</div>";
+	str += "</li>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>총 자리</div>";
+	str += "<div class='col-md-10'>" + data.total + "</div>";
+	str += "<button id='more' class='more'>더보기</button>";
+	str += "</li>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>현재 자리</div>";
+	str += "<div class='col-md-10'>" + data.now + "</div>";
+	str += "</li>";
+	str += "<li class='list-group-item row'>";
+	str += "<div class='col-md-2 title_label'>대기열</div>";
+	str += "<div class='col-md-10'>" + data.wait + "</div>";
+	str += "</li>";
+	str += "</ul>";
+	str += "<a href='update.do?shopNo=" + data.shopNo + "&page=" + pageObeject.page + "&perPageNum=" + pageObject.perPageNum + "&key=" + pageObject.key + "&word=" + pageObject.word + "class='btn btn-default'>수정</a>"; 
+	str += "<a class='btn btn-default' onclick='return false;' data-toggle='modal' data-target='#myModal'>삭제</a>";
+	str += "<a href='list.do?page=" + pageObject.page +"&perPageNum=" + pageObject.perPageNum + "&key=" + pageObject.key + "&word=" + pageObject.word + " class='btn btn-default'>리스트</a>";
+
+	
+	// 댓글 시작 부분
+	str += "<div class='row' style='margin: 20px -30px;'>";
+	str += "<div class='col-lg-12'>";
+	str += "<div class='panel panel-default'>";
+	str += "<div class='panel-heading'>";
+	str += "	<i class='fa fa-comments fa-fw'></i> Reply <br/>";
+	str += "<button class='btn btn-primary btn-xs pull-right' id='writeReplyBtn'>New Reply</button>";
+	str += "</div>";
+	str += "<div class='panel-body'>";
+	str += "<ul class='chat'>";
+	str += "<li class='left clearfix'>";
+	str += "<div>";
+	str += "<div class='header'>";
+	str += "<strong class='primary-font'>user00</strong> <br/>";
+	str += "<small class='pull-right text-muted'>2021.04.21 14:12</small>";
+	str += "</div>";
+	str += "<p>Good job!</p>";
+	str += "<div class='text-right'>";
+	str += "<button class='btn btn-default btn-xs replyUpdateBtn'>수정</button>";
+	str += "<button class='btn btn-default btn-xs replyDeleteBtn'>삭제</button>";
+	str += "</div>";
+	str += "</div>";
+	str += "</li>";
+	str += "</ul>";
+	str += "</div>";
+	// 댓글 panel-body 끝 , 
+	// 댓글 panel 시작
+	str += "<div class='panel-footer'>";
+	str += "<ul class='pagination' id='reply_nav'>";
+	str += "<li><a href=''>1</a></li>";
+	str += "<li class='active'><a href=''>2</a></li>";
+	str += "</ul>";
+	str += "</div>";
+	str += "</div>";
+	str += "</div>";
+	str += "</div>";
+	str += "</div>";
+	
+	// Modal - 리스트 맛집 정보 삭제 시 사용되는 모달 창 
+	str += "<div id='myModal' class='modal fade' role='dialog'>";
+	str += "<div class='modal-dialog'>";
+	str += "<div class='modal-content'>";
+	str += "<button type='button' class='close' data-dismiss='modal'>&times;</button>";
+	str += "<h4 class='modal-title'>맛집 정보 삭제 알림창</h4>";
+	str += "</div>";
+	str += "<div class='modal-body'>";
+	str += "<form action='delete.do' method='post' id='modal_form'>";
+	str += "<input name='shopNo' readonly='readonly' value=" + data.shopNo + ">";
+	str += "<input type='hidden' name='perPageNum' value=" + pageObject.perPageNum + ">";
+	str += "</form>";
+	str += "</div>";
+	str += "<div class='modal-footer'>";
+	str += "<button type='button' class='btn btn-default' id='modal_deleteBtn'>삭제</button>";
+	str += "<button type='button' class='btn btn-default' data-dismiss='modal'>취소</button>";
+	str += "</div>";
+	str += "</div>";
+	str += "</div>";
+	str += "</div>";
+	// <!-- Modal - 리스트 맛집 삭제 시 사용되는 모달 창의 끝 -->
+	
+	// <!-- Modal - 댓글 쓰기/ 수정 시 사용되는 모달창 -->
+	
+	str += "<div id='replyModal' class='modal fade' role='dialog'>";
+	str += "<div class='modal-dialog'>";
+	str += "<div class='modal-content'>";
+	str += "<div class='modal-header'>";
+	str += "<button type='button' class='close' data-dismiss='modal'>&times;</button>";
+	str += "<h4 class='modal-title'>";
+	str += "<i class='fa fa-comments fa-fw'></i> <span id='replyModalTitle'>Reply Modal</span>";
+	str += "</h4>";
+	str += "</div>";
+	str += "<div class='modal-body'>";
+	
+	str += "<form>";
+	str += "<div class='form-group' id='replyNoDiv'>";
+	str += "<label for='replyNo'>댓글 번호:</label>";
+	str += "<input name='replyNo' type='text' class='form-control' id='replyNo' readonly='readonly' >";
+	str += "</div>";
+	str += "<div class='form-group' id='replyshopNoDiv'>";
+	str += "<label for='replyshopNo'>맛집 등록 번호:</label>";
+	str += "<input name='shopNo' type='text' class='form-control' id='replyshopNo' readonly='readonly' value=" +data.shopNo + ">";
+	str += "</div>";
+	str += "<div class='form-group' id='replyshopNameDiv'>";
+	str += "<label for='replyshopName'>맛집 이름:</label>";
+	str += "<input name='shopName' type='text' class='form-control' id='replyshopName' readonly='readonly' value=" + data.shopName + ">";
+	str += "</div>";
+	str += "<div class='form-group' id='replyContentDiv'>";
+	str += "<label for='replyContent'>내용:</label>";
+	str += "<textarea name='content' class='form-control' rows='5' id='replyContent' required='required'></textarea>";
+	str += "</div>";
+	str += "<div class='form-group' id='replyWriterDiv'>";
+	str += "<label for='replyWriter'>아이디:</label>";
+	str += "<input name='writer' type='text' class='form-control' id='replyWriter' required='required' value=" + login.id + ">";
+	str += "</div>";
+	str += "</form>";
+	str += "</div>";
+	str += "<div class='modal-footer'>";
+	str += "<button type='button' class='btn btn-default' id='replyModalWriteBtn'>등록</button>";
+	str += "<button type='button' class='btn btn-default' id='replyModalUpdateBtn'>수정</button>";
+	str += "<button type='button' class='btn btn-default' id='replyModalDeleteBtn'>삭제</button>";
+	str += "<button type='button' class='btn btn-default' data-dismiss='modal'>취소</button>";
+	str += "</div>";
+	str += "</div>";
+	str += "</div>";
+	str += "</div>";
+	str += "</body>";
+
